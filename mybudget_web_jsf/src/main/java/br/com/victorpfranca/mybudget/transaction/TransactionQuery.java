57e71fe2bdbca38b83cee016335d488b1c518ca1
Victor Franca <victorhugof@gmail.com>
2019-04-25 22:05:16 -0300

    a

--- mybudget_web_jsf/src/main/java/br/com/victorpfranca/mybudget/transaction/TransactionQuery.java
+++ mybudget_web_jsf/src/main/java/br/com/victorpfranca/mybudget/transaction/TransactionQuery.java
@@ -1,11 +1,16 @@
 package br.com.victorpfranca.mybudget.transaction;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 
+import br.com.victorpfranca.mybudget.InOut;
+import br.com.victorpfranca.mybudget.account.Account;
+import br.com.victorpfranca.mybudget.category.Category;
 import br.com.victorpfranca.mybudget.view.MonthYear;
 
 @RequestScoped
@@ -14,7 +19,21 @@ public class TransactionQuery implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	public List<Transaction> transactions(TransactionsFilter transactionsFilter) {
-		return new ArrayList<Transaction>();
+
+		List<Transaction> transactions = new ArrayList<Transaction>();
+		for (int i = 0; i < 25; i++) {
+			CheckingAccountTransaction transaction = new CheckingAccountTransaction();
+			transaction.setStatus(TransactionStatus.CONFIRMADO);
+			transaction.setData(Calendar.getInstance().getTime());
+			transaction.setContaDestino(new Account("Banco do Brasil"));
+			transaction.setCategoria(new Category("Categoria Teste", InOut.E));
+			transaction.setValor(BigDecimal.TEN);
+			transaction.setInOut(InOut.E);
+			transaction.setSaldo(BigDecimal.TEN);
+			transactions.add(transaction);
+		}
+
+		return transactions;
 	}
 
 	public List<Transaction> extratoCartao(TransactionsFilter transactionsFilter) {
